c5a39e82b883e741fdc95653a3604aad55045120
Pamela Carriel <pjcarriel@espe.edu.ec>
2023-05-15 10:15:51 -0500

    Codigo de lista

---
+++ Lista.py
@@ -0,0 +1,22 @@
+# Crear una lista vacía
+mi_lista = []
+
+# Agregar elementos a la lista
+mi_lista.append(1)
+mi_lista.append(2)
+mi_lista.append(3)
+
+# Imprimir la lista
+print(mi_lista)
+
+# Acceder a un elemento de la lista
+print(mi_lista[0])
+
+# Modificar un elemento de la lista
+mi_lista[0] = 4
+
+# Eliminar un elemento de la lista
+del mi_lista[2]
+
+# Imprimir la lista actualizada
+print(mi_lista)
