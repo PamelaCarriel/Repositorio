0554617a668434d15a240e20bf7c48a38a5f2fb9
PamelaCarriel <117676378+PamelaCarriel@users.noreply.github.com>
2023-06-02 10:36:25 -0500

    Evaluación

--- Prueba Formativa/Suma.py
+++ Prueba Formativa/Suma.py
@@ -1,5 +1,4 @@
 #Universidad de las Fuerzas Armadas - ESPE
-#Una estructura de datos adecuada para implementar una Pila
 #Autor: Pamela Jesabel Carriel Mier
 #Fecha: 02 de junio del 2023
 result = 2 + 2 * 3
